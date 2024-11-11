abfb6323d949a7e5399c259417346a7aaa270a9f
Fareeha Noor <fareehaabbas21@gmail.com>
2024-11-10 19:35:44 -0800

    feat: About section done

--- src/components/heroSection/Hero.jsx
+++ src/components/heroSection/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
           <div className="flex items-center gap-3 ">
             <figure className="img-box w-9 h-9 rounded-lg">
               <img
-                src="/images/1.png"
+                src="/images/fareeha-pic.svg"
                 alt="image"
                 width={40}
                 height={40}
