7690070a3bbcacd5c866a86a25ee934277f03a27
Fareeha Noor <fareehaabbas21@gmail.com>
2024-11-16 00:30:12 -0800

    feat: add contact form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
 import Header from "./components/Header/Header";
 import Hero from "./components/heroSection/Hero";
 import Review from "./components/Review/Review";
@@ -15,6 +16,7 @@ const App = () => {
         <Skill />
         <Work />
         <Review />
+        <Contact />
       </main>
     </>
   );
